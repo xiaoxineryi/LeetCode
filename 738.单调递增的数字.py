e66ab310eef537da123906e17165a34648c511b5
xiaoxineryi <529086017@qq.com>
2020-08-28 21:52:42 +0800

    2020-08-27

---
+++ 738.单调递增的数字.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode.cn id=738 lang=python3
+#
+# [738] 单调递增的数字
+#
+
+# @lc code=start
+class Solution:
+    def monotoneIncreasingDigits(self, N: int) -> int:
+        
+# @lc code=end
+
